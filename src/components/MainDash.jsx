ce8ffe577cd2fe6dcbea09aa7095d61083361001
Kene <kayyungx@gmail.com>
2023-01-26 07:35:49 +0100

    Add sales overview component

--- src/components/MainDash.jsx
+++ src/components/MainDash.jsx
@@ -1,9 +1,11 @@
 import MainDashNav from './MainDashNav';
+import SalesOverview from './SalesOverview';
 
 const MainDash = () => {
   return (
     <div className="bg-[#9aaedd34] rounded-[2rem] mt-4">
       <MainDashNav />
+      <SalesOverview />
     </div>
   );
 };
